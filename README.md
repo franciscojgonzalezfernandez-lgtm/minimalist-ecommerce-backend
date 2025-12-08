f9b49046b6f82df37a291ff57633da37d1237698
Francisco Javier González <franciscojgonzalezfernandez@gmail.com>
2025-12-08 15:52:16 +0100

    Readme update.

--- README.md
+++ README.md
@@ -2,19 +2,19 @@
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
 </p>
 
-# Teslo API
+# Minimalist e-commerce API
 
-1. Clonar proyecto
+1. Clone the project
 2. `yarn install`
-3. Clonar el archivo `.env.template` y renombrarlo a `.env`
-4. Cambiar las variables de entorno
-5. Levantar la base de datos
+3. Clone the file `.env.template` and rename to `.env`
+4. Change the environment variables
+5. set up the data base.
 
 ```
 docker-compose up -d
 ```
 
-6. Levantar:
+6. Get up the server:
 
 ```
 npm run start:dev
@@ -22,7 +22,7 @@ yarn start:dev
 
 ```
 
-7. Ejecutar SEED
+7. Execute SEED
 
 ```
 http://localhost:3000/api/seed
--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,50 +1,57 @@
 import { JwtService } from '@nestjs/jwt';
-import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { JwtPayload } from '../auth/interfaces';
 import { NewMessageDto } from './dtos/new-message.dto';
 import { MessagesWsService } from './messages-ws.service';
 
 @WebSocketGateway({ cors: true })
-export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  
+export class MessagesWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer() wss: Server;
 
   constructor(
     private readonly messagesWsService: MessagesWsService,
-    private readonly jwtService: JwtService
+    private readonly jwtService: JwtService,
   ) {}
 
-  async handleConnection( client: Socket ) {
+  async handleConnection(client: Socket) {
     const token = client.handshake.headers.authentication as string;
     let payload: JwtPayload;
 
     try {
-      payload = this.jwtService.verify( token );
-      await this.messagesWsService.registerClient( client, payload.id );
-
+      payload = this.jwtService.verify(token);
+      await this.messagesWsService.registerClient(client, payload.id);
     } catch (error) {
       client.disconnect();
       return;
     }
 
-    // console.log({ payload })    
-    // console.log('Cliente conectado:', client.id );
-    
-
-    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients() );
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
   }
 
-  handleDisconnect( client: Socket ) {
+  handleDisconnect(client: Socket) {
     // console.log('Cliente desconectado', client.id )
-    this.messagesWsService.removeClient( client.id );
+    this.messagesWsService.removeClient(client.id);
 
-    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients() );
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
   }
 
   @SubscribeMessage('message-from-client')
-  onMessageFromClient( client: Socket, payload: NewMessageDto ) {
-  
+  onMessageFromClient(client: Socket, payload: NewMessageDto) {
     //! Emite únicamente al cliente.
     // client.emit('message-from-server', {
     //   fullName: 'Soy Yo!',
@@ -59,10 +66,7 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
 
     this.wss.emit('message-from-server', {
       fullName: this.messagesWsService.getUserFullName(client.id),
-      message: payload.message || 'no-message!!'
+      message: payload.message || 'no-message!!',
     });
-
   }
-
-
 }
